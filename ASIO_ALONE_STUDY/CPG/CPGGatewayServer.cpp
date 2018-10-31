effd27ec9a809d971e84178083b3b770c29af44a
fenggeren <huanao@huanaodemacbook-pro.local>
2018-10-31 10:05:16 +0800

    hh

--- ASIO_ALONE_STUDY/CPG/CPGGatewayServer.cpp
+++ ASIO_ALONE_STUDY/CPG/CPGGatewayServer.cpp
@@ -8,19 +8,28 @@
 
 #include "CPGGatewayServer.hpp"
 #include "Conv.hpp"
+#include "DataBuffer.hpp"
+#include "TCPSession.hpp"
 
 using namespace std::placeholders;
 CPGGatewayServer::CPGGatewayServer()
 {
-    server_ = std::make_shared<TCPServer>(getIoContext(), 5578);
-    server_->setMessageCallback(std::bind(&CPGGatewayServer::messageCallback, this, _1, _2));
+    server_ = std::make_shared<TCPServer>(getIoContext(), 7237);
+//    server_->setMessageCallback(std::bind(&CPGGatewayServer::messageCallback, this, _1, _2));
     server_->setConnectionCallback(std::bind(&CPGGatewayServer::connectionCallback, this, _1));
     server_->setRemoveConnectionCallback(std::bind(&CPGGatewayServer::connectionCallback, this, _1));
 }
 
-void CPGGatewayServer::messageCallback(const std::shared_ptr<TCPSession>&, DataBuffer*const)
+void CPGGatewayServer::start()
 {
-    
+    server_->start();
+    getIoContext().run();
+}
+
+void CPGGatewayServer::messageCallback(const std::shared_ptr<TCPSession>& session, DataBuffer*const data)
+{
+    data->retrieveAll();
+    session->forceClose();
 }
 
 void CPGGatewayServer::connectionCallback(const std::shared_ptr<TCPSession>&)
